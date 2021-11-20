d029caf358c891bcf48efd18d40976436468505d
Isa Thais <isaalexandre>
2021-11-20 19:51:11 -0300

    feat:endpoint for editing a recipe and endpoint for deleting a recipe

--- src/api/recipes/recipesController.js
+++ src/api/recipes/recipesController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
 const Recipe = require('../models/recipe');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -25,15 +26,9 @@ router.use(authMiddleware);
 
 router.post('/', async (request, response) => {
   try {
-    const { name, ingredients, preparation } = request.body;
-
-    const { userId } = request;
-
     const recipe = await Recipe.create({
-      name,
-      ingredients,
-      preparation,
-      userId,
+      ...request.body,
+      userId: request.userId,
     });
 
     return response.status(201).json({ recipe });
@@ -41,6 +36,49 @@ router.post('/', async (request, response) => {
     return response
       .status(400)
       .send({ message: 'Invalid entries. Try again.' });
+  }
+});
+
+router.put('/:_id', async (request, response) => {
+  try {
+    const { _id } = request.params;
+    const userId = request.userId;
+
+    const user = await User.findById(userId);
+    const recipe = await Recipe.findById(_id);
+    const data = { ...request.body, userId };
+
+    if (user.role !== 'admin' && recipe.userId !== request.userId) {
+      return response.status(401).send({ message: 'missing auth token' });
+    }
+
+    const recipeUpdate = await Recipe.findOneAndUpdate(_id, data, {
+      new: true,
+    });
+
+    return response.status(200).json(recipeUpdate);
+  } catch (err) {
+    return response.status(401).send({ message: 'Error edit project' });
+  }
+});
+
+router.delete('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const userId = request.userId;
+
+    const user = await User.findById(userId);
+    const recipe = await Recipe.findById(id);
+
+    if (user.role !== 'admin' && recipe.userId !== request.userId) {
+      return response.status(401).send({ message: 'missing auth token' });
+    }
+
+    await Recipe.findByIdAndRemove(id);
+
+    return response.status(204).send();
+  } catch (err) {
+    return response.status(401).send({ message: 'Error deleting project.' });
   }
 });
 
